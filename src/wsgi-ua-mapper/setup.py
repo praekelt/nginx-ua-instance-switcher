0890d12d297a9b7064be5d113ad5a4712a22c8c8
shaunsephton <shaun@praekeltconsulting.com>
2011-02-18 09:41:07 +0200

    first commit

--- src/wsgi-ua-mapper/setup.py
+++ src/wsgi-ua-mapper/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-ua-mapper',
-    version='0.0.9',
-    description='Django management command mapping User-Agent header strings to user defined values. Resulting User-Agent:value set is stored in cache.',
+    name='wsgi-ua-mapper',
+    version='0.0.1',
+    description='WSGI app mapping requesting User Agent to arbitrary value through user defined module. Utilizes Wurfl to deliver requesting device info.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
     author_email='dev@praekelt.com',
     license='BSD',
-    url='http://github.com/praekelt/django-ua-mapper',
+    url='http://github.com/praekelt/wsgi-ua-mapper',
     packages = find_packages(),
     include_package_data=True,
     classifiers = [
@@ -16,13 +16,11 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
-        "Framework :: Django",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     install_requires = [
         'pywurfl',
-        'djanginxed'
     ],
     zip_safe=False,
 )
