48620d1b9ccdeb100ec4de57d246731c2adf34cd
shaunsephton <shaunsephton@gmail.com>
2011-03-24 11:18:32 +0200

    update to match article

--- src/example-mapper/mapper/wsgi.py
+++ src/example-mapper/mapper/wsgi.py
@@ -2,12 +2,9 @@ from ua_mapper.wsgi import UAMapper
 
 class MyMapper(UAMapper):
     def map(self, device):
-        """
-        Override this method to perform your own custom mapping.
-        """
-        if device.resolution_width < 240:
-            return 'medium'
+        if device.resolution_width < 500:
+            return 'mobile'
         else:
-            return 'high'
+            return 'desktop'
     
 application = MyMapper()
